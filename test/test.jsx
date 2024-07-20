7b5fad3acfedd9e916a652bc7e56d042abbe2bab
nandesh-dev <nandesh.dev@gmail.com>
2024-07-20 14:43:19 +0530

    Add code formater prettier

--- test/test.jsx
+++ test/test.jsx
@@ -1,8 +1,10 @@
 export function Nav() {
-  return /*cat:wmue*/<ol>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-  </ol>;
+  return (
+    /*cat:wmue*/ <ol>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ol>
+  );
 }
--- test/test2.jsx
+++ test/test2.jsx
@@ -1,30 +1,40 @@
 import { Nav } from "./test2";
 export function App() {
   const name = true;
-  return /*cat:bc55*/<div>
-    <Nav />
-    {/*cat:6mq6*/[1, 2, 3].map(num => {
-      return /*cat:f70h*/<p> {/*cat:yk9o*/num}</p>;
-    })}
-    <h1>Hello World</h1>
-    {/*cat:bra9*/name ? /*cat:mj87*/<div>name</div> : /*cat:kcuu*/<div>noname</div>}
-    <div>
-      <p>
-        rj1 209re1hdf9013jf01j9cn1309cn10nc03 03rj1f08c 130r\ n 183f01 cn3
-      </p>
-    </div>
-    <div>
+  return (
+    /*cat:bc55*/ <div>
+      <Nav />
+      {
+        /*cat:6mq6*/ [1, 2, 3].map((num) => {
+          return /*cat:f70h*/ <p> {/*cat:yk9o*/ num}</p>;
+        })
+      }
+      <h1>Hello World</h1>
+      {
+        /*cat:bra9*/ name ? (
+          /*cat:mj87*/ <div>name</div>
+        ) : (
+          /*cat:kcuu*/ <div>noname</div>
+        )
+      }
+      <div>
+        <p>
+          rj1 209re1hdf9013jf01j9cn1309cn10nc03 03rj1f08c 130r\ n 183f01 cn3
+        </p>
+      </div>
       <div>
         <div>
           <div>
             <div>
               <div>
-                <div></div>
+                <div>
+                  <div></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
